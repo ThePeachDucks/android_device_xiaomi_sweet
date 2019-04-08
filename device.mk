63dc0290766c41aea29d228dba3af7c09aac8d35
Bowgo Tsai <bowgotsai@google.com>
2019-04-08 18:28:48 +0800

    sweet: Inherit gsi_keys.mk
    Bug:112293933
    Test: tree hugger
    Change-Id: I112ece91566e6cea066cd637bfed5cae6761ae66

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/sweet/sweet-vendor.mk)
 
+# Install gsi keys into ramdisk, to boot a GSI with verified boot
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Recovery
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom \
