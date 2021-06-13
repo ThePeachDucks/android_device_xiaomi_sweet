d408d9896bf93d948985dd4acb05213fa6f54bdc
Albert I <kras@raphielgang.org>
2021-06-13 16:53:13 +0800

    sweet: Set default hotspot SSID based on HWC property
    Change-Id: Ib197c3a318d61208dc3f114a8c6f96a542cfcf5d
    Signed-off-by: Albert I <kras@raphielgang.org>

--- device.mk
+++ device.mk
@@ -268,7 +268,9 @@ PRODUCT_PACKAGES += \
     SweetSettings \
     SweetSystemUI \
     FrameworksResTarget \
-    WifiResTarget
+    WifiResTarget \
+    WifiOverlayM2101K6G \
+    WifiOverlayM2101K6I
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
