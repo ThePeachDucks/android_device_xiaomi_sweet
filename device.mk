18c5f9a4a1e46bfbd2b8d67575b4ef7076c9e296
sweetyicecare <gabrielseedev@outlook.com>
2022-04-02 18:15:32 +0530

    sweet: Set up recovery filesystem
    Signed-off-by: sweetyicecare <gabrielseedev@outlook.com>

--- device.mk
+++ device.mk
@@ -10,3 +10,8 @@ $(call inherit-product, vendor/xiaomi/sweet/sweet-vendor.mk)
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Recovery
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom
