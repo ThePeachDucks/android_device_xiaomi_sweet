c357c1cdec5c8dd0bed81b2da51b7d7a9fe1adb1
ArianK16a <arian.kulmer@web.de>
2022-04-02 18:29:04 +0530

    sweet: Ship device API level
    Signed-off-by: sweetyicecare <gabrielseedev@outlook.com>

--- device.mk
+++ device.mk
@@ -16,6 +16,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# API level
+PRODUCT_SHIPPING_API_LEVEL := 30
+
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
