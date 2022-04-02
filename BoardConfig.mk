78285f48b0ccdc6a276ce672ede25087572f6fe9
sweetyicecare <gabrielseedev@outlook.com>
2022-04-02 17:01:06 +0530

    sweet: Add ARCH and Bootloader flags
    Signed-off-by: sweetyicecare <gabrielseedev@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,3 +10,19 @@ DEVICE_PATH := device/xiaomi/sweet
 
 # Inherit from proprietary files
 include vendor/xiaomi/sweet/BoardConfigVendor.mk
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := cortex-a76
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a76
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := sm6150
+TARGET_NO_BOOTLOADER := true
