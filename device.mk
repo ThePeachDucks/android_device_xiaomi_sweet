c6cff2071c052a14bbb515c698b063b005a9b8ed
sweetyicecare <gabrielseedev@outlook.com>
2022-04-02 16:49:56 +0530

    sweet: Import vendor blobs from LA.UM.9.1.r1-11500.02-SMxxx0.QSSI12.0
    Signed-off-by: sweetyicecare <gabrielseedev@outlook.com>

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Call the proprietary setup
+$(call inherit-product, vendor/xiaomi/sweet/sweet-vendor.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
