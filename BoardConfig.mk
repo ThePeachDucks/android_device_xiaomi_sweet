ffe8fbc6c8bc097356eb17d05f84827e3bbe432a
Kuba Wojciechowski <nullbytepl@gmail.com>
2022-04-09 12:46:47 +0530

    sweet: Disable kpti on kernel
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>
    Signed-off-by: sweetyicecare <gabrielseedev@outlook.com>
    Change-Id: Ica465f586a44129047c266597316045fab762550

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,6 +44,7 @@ BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=1
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
+BOARD_KERNEL_CMDLINE += kpti=off
 
 BOARD_BOOT_HEADER_VERSION := 2
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
